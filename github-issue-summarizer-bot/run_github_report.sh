808e6f3c0f82f8a1322166c8a8742977b58aedda
Ryan McConville <ryan@ryanmcconville.com>
2024-11-24 07:36:52 +0000

    add support for debug mode (dont post to slack)

--- github-issue-summarizer-bot/run_github_report.sh
+++ github-issue-summarizer-bot/run_github_report.sh
@@ -6,6 +6,7 @@ BASE_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Default values
 CONFIG_FILE="$BASE_DIR/config.yaml"
 ENV_FILE=""
+DEBUG=""
 
 while [[ $# -gt 0 ]]; do
     case $1 in
@@ -17,9 +18,13 @@ while [[ $# -gt 0 ]]; do
             ENV_FILE="$2"
             shift 2
             ;;
+        --debug)
+            DEBUG="--debug"
+            shift
+            ;;
         *)
             echo "Unknown option: $1"
-            echo "Usage: $0 [--config config.yaml] [--env .env]"
+            echo "Usage: $0 [--config config.yaml] [--env .env] [--debug]"
             exit 1
             ;;
     esac
@@ -39,10 +44,14 @@ CMD="python3 main.py --config \"$CONFIG_FILE\""
 if [ -n "$ENV_FILE" ]; then
     CMD="$CMD --env \"$ENV_FILE\""
 fi
+if [ -n "$DEBUG" ]; then
+    CMD="$CMD --debug"
+fi
 
 # Run the script
 echo "Running with config: $CONFIG_FILE"
 [ -n "$ENV_FILE" ] && echo "Using env file: $ENV_FILE"
+[ -n "$DEBUG" ] && echo "Debug mode enabled"
 eval "$CMD" >> klusterai_github_bot_report.log 2>&1
 
 # Deactivate virtual environment
